52cc016a5afdc9fec1bedf570e60cfc4e548e560
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-11-03 14:27:35 +0100

    NEW : Added Human class

---
+++ src/Model/Human.class.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Model;
+
+abstract class Human
+{
+    private string $name;
+    private string $address;
+    private string $street;
+    private string $postcode;
+    private string $city;
+    private Categorie $categorie;
+
+
+    public function __construct(string $name, string $address, string $street, string $postcode, string $city, Categorie $categorie)
+    {
+        $this->setName($name)
+            ->setAddress($address)
+            ->setStreet($street)
+            ->setPostcode($postcode)
+            ->setCity($city)
+            ->setCategorie($categorie);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Human
+     */
+    public function setName(string $name): Human
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     * @return Human
+     */
+    public function setAddress(string $address): Human
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     * @return Human
+     */
+    public function setStreet(string $street): Human
+    {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostcode(): string
+    {
+        return $this->postcode;
+    }
+
+    /**
+     * @param string $postcode
+     * @return Human
+     */
+    public function setPostcode(string $postcode): Human
+    {
+        $this->postcode = $postcode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return Human
+     */
+    public function setCity(string $city): Human
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return Categorie
+     */
+    public function getCategorie(): Categorie
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * @param Categorie $categorie
+     * @return Human
+     */
+    public function setCategorie(Categorie $categorie): Human
+    {
+        $this->categorie = $categorie;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return "Détail de l'Humain :\n
+         Nom : {$this->getName()}\n
+         Adresse : {$this->getAddress()}\n
+         Rue : {$this->getStreet()}\n
+         Code postal : {$this->getPostcode()}\n
+         Ville : {$this->getCity()}\n
+         Catégorie : {$this->getCategorie()}\n";
+    }
+
+
+}
