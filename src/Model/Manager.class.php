f6f0cf9d28b74b9cedab5cc3ed694105e9b6b058
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-11-03 15:18:42 +0100

    EDIT : Change to the name of the Stable namespaces and remove $categorie in the construct parameters

--- src/Model/Manager.class.php
+++ src/Model/Manager.class.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 class Manager extends Human
 {
-    public function __construct(string $name, string $address, string $street, string $postcode, string $city, Categorie $categorie)
+    public function __construct(string $name, string $address, string $street, string $postcode, string $city)
     {
         parent::__construct($name, $address, $street, $postcode, $city, new ManagerCategorie());
     }
