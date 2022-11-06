745f900bc4e967775b5bf0047def341a4bf8364c
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-11-06 19:00:59 +0100

    ADD : The expected result of a test

--- src/app.php
+++ src/app.php
@@ -16,19 +16,21 @@ use App\Controller\ExecWithoutError;
 use App\Controller\ExecWithSameHorseEvent;
 
 
+//Il est prévu dans ce test qu'aucun soucis d'initilisation ne se produise et qu'un évenement soit lancé
 $test1= new ExecWithoutError();
 $test1->execute();
 
 
-//echo "Nous allons maintenant tester l'erreur de réinscription d'un cheval à un événement \n";
+//Il est prévu dans ce test qu'un soucis d'initilisation se produise en raison de la réinscription d'un cheval
 //$test2= new ExecWithSameHorseEvent();
 //$test2->execute();
 
-//echo "Nous allons maintenant tester l'erreur de dépassement du nombre de chevaux inscrits à un événement \n";
+//Il est prévu dans ce test qu'un soucis d'initilisation se produise en raison d'un autre de participation maximal atteint
 //$test3= new ExecWithMaxCommitments();
 //$test3->execute();
 
-//echo "Nous allons maintenant tester l'erreur de dépassement du l'eau disponible à l'évenement \n";
+
+//Il est prévu dans ce test qu'un soucis d'initilisation se produise en raison d'une incapacité par l'évenement à fournir de l'eau
 //$test4= new ExecWithNoMoreWater();
 //$test4->execute();
 
