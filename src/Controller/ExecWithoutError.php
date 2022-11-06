f2f019cae756bda607a4584cfd15d509ba20bf3b
Dimitri Perriet <dimitri.perriet@outlook.frls>
2022-11-06 18:20:51 +0100

    ADD : Controller classes & main class

---
+++ src/Controller/ExecWithoutError.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Manager;
+use App\Model\Stable;
+
+class ExecWithoutError
+{
+    public function execute()
+    {
+        // On instancie notre application
+
+        //Création d'un manager
+        $manager= new Manager("Dimitri","49 Avenue de la côte de Nacre","Cote","14000","Caen");
+        echo $manager;
+
+        //Création d'un stable
+        $ecurie= new Stable("Ecurie de la Plaine", "Rue de la Plaine", "1", "75000", "Paris", $manager);
+        echo $ecurie;
+
+        //Création d'un cavalier
+        $rider=new \App\Model\Rider("Julien","Rue de la Plaine","1","75000","Paris", new \App\Model\Saut());
+        echo $rider;
+
+        //Création de chevaux, poney et Shetiland
+        $cheval1 = new \App\Model\Horse("001-d-B-1", "blanc", 500, $rider);
+        $cheval2 = new \App\Model\Poney("002-d-B-2", "blanc", 200, $rider);
+        $cheval3 = new \App\Model\Sheitland("003-d-B-3", "blanc", 501, $rider);
+
+        echo $cheval1;
+        echo $cheval2;
+        echo $cheval3;
+
+        //Création d'un événement
+        $event1 = new \App\Model\Testevent(2, 1000);
+        echo $event1;
+
+        //Inscription d'un cheval à un événement
+        $event1->subscribeHorse($cheval1);
+        echo $event1;
+        $event1->subscribeHorse($cheval2);
+        echo $event1;
+    }
+}
--- src/app.php
+++ src/app.php
@@ -10,51 +10,31 @@ define('TEMPLATES_DIR', __DIR__ . '/../templates/');
 define('SRC_DIR', __DIR__ . '/');
 define('PUBLIC_DIR', __DIR__ . '/../public/');
 
-use App\Model\Manager;
-use App\Model\Stable;
+use App\Controller\ExecWithMaxCommitments;
+use App\Controller\ExecWithNoMoreWater;
+use App\Controller\ExecWithoutError;
+use App\Controller\ExecWithSameHorseEvent;
 
-// On instancie notre application
 
-//Création d'un manager
-$manager= new Manager("Dimitri","49 Avenue de la côte de Nacre","Cote","14000","Caen");
-echo $manager;
+$test1= new ExecWithoutError();
+$test1->execute();
 
-//Création d'un stable
-$ecurie= new Stable("Ecurie de la Plaine", "Rue de la Plaine", "1", "75000", "Paris", $manager);
-echo $ecurie;
 
-//Création d'un cavalier
-$rider=new \App\Model\Rider("Julien","Rue de la Plaine","1","75000","Paris", new \App\Model\Saut());
-echo $rider;
+//echo "Nous allons maintenant tester l'erreur de réinscription d'un cheval à un événement \n";
+//$test2= new ExecWithSameHorseEvent();
+//$test2->execute();
 
-//Création de chevaux, poney et Shetiland
-$cheval1 = new \App\Model\Horse("1", "blanc", 500, $rider);
-$cheval2 = new \App\Model\Poney("2", "blanc", 200, $rider);
-$cheval3 = new \App\Model\Sheitland("3", "blanc", 501, $rider);
+//echo "Nous allons maintenant tester l'erreur de dépassement du nombre de chevaux inscrits à un événement \n";
+//$test3= new ExecWithMaxCommitments();
+//$test3->execute();
 
-echo $cheval1;
-echo $cheval2;
-echo $cheval3;
+echo "Nous allons maintenant tester l'erreur de dépassement du l'eau disponible à l'évenement \n";
+$test4= new ExecWithNoMoreWater();
+$test4->execute();
 
-//Création d'un événement
-$event1 = new \App\Model\Testevent(2, 1000);
-echo $event1;
 
-//Inscription d'un cheval à un événement
-$event1->subscribeHorse($cheval1);
-echo $event1;
-$event1->subscribeHorse($cheval2);
 
-//Tentative de réinscription d'un cheval à un événement : ERREUR
-//$event1->subscribeHorse($cheval2);
 
-//Tentative d'inscription d'un cheval à un événement alors que le nombre maximum de chevaux est atteint : ERREUR
-//$event1->subscribeHorse($cheval3);
-
-$event2 = new \App\Model\Testevent(2, 600);
-//Tentative d'inscription alors qu'il n'y a pas assez d'eau : ERREUR
-$event2->subscribeHorse($cheval1);
-$event2->subscribeHorse($cheval2);
 
 
 
